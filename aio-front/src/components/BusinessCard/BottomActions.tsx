62b1da7519c84b9075c840ea09af5089a4ed5bcb
RodinHatokayTe <rodinhatokay1998@gmail.com>
2023-05-22 12:31:48 +0300

    added favorites screen

--- aio-front/src/components/BusinessCard/BottomActions.tsx
+++ aio-front/src/components/BusinessCard/BottomActions.tsx
@@ -6,6 +6,9 @@ import FeatherIcon from "react-native-vector-icons/Feather";
 import OcticonsIcon from "react-native-vector-icons/Octicons";
 import { useTheme } from "../../contexts/ThemeContext";
 
+const iconSize = 22;
+const hitSlop = 20;
+
 interface Props {
 	handleCall: VoidFunction;
 	handleWhatsApp: VoidFunction;
@@ -25,19 +28,25 @@ const BottomActions: FC<Props> = ({
 	return (
 		<View style={styles.buttonContainer}>
 			<IconButton
-				icon={() => <FeatherIcon color={color} name="phone" size={22} />}
+				hitSlop={hitSlop}
+				icon={() => <FeatherIcon color={color} name="phone" size={iconSize} />}
 				onPress={handleCall}
 			/>
 			<IconButton
-				icon={() => <Icon name="whatsapp" color={color} size={22} />}
+				hitSlop={hitSlop}
+				icon={() => <Icon name="whatsapp" color={color} size={iconSize} />}
 				onPress={handleWhatsApp}
 			/>
 			<IconButton
-				icon={() => <OcticonsIcon name="person-add" color={color} size={22} />}
+				hitSlop={hitSlop}
+				icon={() => (
+					<OcticonsIcon name="person-add" color={color} size={iconSize} />
+				)}
 				onPress={handleSaveContact}
 			/>
 			<IconButton
-				icon={() => <FeatherIcon name="share" color={color} size={22} />}
+				hitSlop={hitSlop}
+				icon={() => <FeatherIcon name="share" color={color} size={iconSize} />}
 				onPress={handleShare}
 			/>
 		</View>
--- aio-front/src/screens/FavoritesScreen.tsx
+++ aio-front/src/screens/FavoritesScreen.tsx
@@ -1,7 +1,46 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader/Loader";
+import { Business } from "../services/business/business.types";
+import BusinessCard from "../components/BusinessCard/BusinessCard";
+import EmptyFavorites from "../sections/Favorites/EmptyFavorites";
+import { favoriteBusinesses } from "../mock/businsesses";
 
 const FavoritesScreen = () => {
-	return <View></View>;
+	const [loading, setLoading] = useState(true);
+
+	const [favorites, setFavorites] = useState<Business[]>(favoriteBusinesses);
+
+	const renderItem = ({ item }: { item: Business }) => (
+		<BusinessCard business={item} />
+	);
+
+	useEffect(() => {
+		// temp for ui
+		// fetch favorites here..
+		setTimeout(() => {
+			setLoading(false);
+		}, 1500);
+	}, []);
+
+	if (loading) return <Loader loadingScreen />;
+
+	return (
+		<View style={styles.flex}>
+			<FlatList
+				data={favorites}
+				renderItem={renderItem}
+				style={styles.flex}
+				keyExtractor={(item) => item.id}
+				ListEmptyComponent={EmptyFavorites}
+			/>
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	flex: { flex: 1 },
+});
 
 export default FavoritesScreen;
