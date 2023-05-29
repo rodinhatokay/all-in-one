5b8417b0ca3653885e54be4df1ca089482e19e92
rodinhatokay <57593612+rodinhatokay@users.noreply.github.com>
2023-05-29 21:57:38 +0300

    Update app.module.ts

--- aio-back/src/app.module.ts
+++ aio-back/src/app.module.ts
@@ -1,27 +1,29 @@
-import * as Joi from '@hapi/joi';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { CommonModule } from './common/common.module';
-import appConfig from './config/app.config';
-import { UsersModule } from './users/user.module';
-import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { BusinessModule } from './business/business.module';
+import * as Joi from "@hapi/joi";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { CommonModule } from "./common/common.module";
+import appConfig from "./config/app.config";
+import { UsersModule } from "./users/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
+import { BusinessModule } from "./business/business.module";
+
+console.log("process.env.DATABASE_HOSE", process.env.DATABASE_HOSE);
 
 @Module({
 	imports: [
 		TypeOrmModule.forRootAsync({
 			useFactory: () => ({
-				type: 'postgres',
-				host: '127.0.0.1',
+				type: "postgres",
+				host: "127.0.0.1",
 				post: 5432,
-				username: 'postgres',
-				password: 'pass123',
-				database: 'postgres',
+				username: "postgres",
+				password: "pass123",
+				database: "postgres",
 				autoLoadEntities: true,
 				synchronize: true,
 			}),
@@ -30,8 +32,6 @@ import { BusinessModule } from './business/business.module';
 			validationSchema: Joi.object({
 				DATABASE_HOST: Joi.required(),
 				DATABASE_PORT: Joi.number().default(5432),
-				JWT_KEY: Joi.string().required(),
-				SESSION_KEY: Joi.string().required(),
 			}),
 			load: [appConfig],
 		}),
