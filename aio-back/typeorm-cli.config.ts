a0489d5a4f53125b9f3de31c475da8003af979c1
Aslan Shapso <>
2023-06-10 16:18:36 +0300

    Reverting

--- aio-back/typeorm-cli.config.ts
+++ aio-back/typeorm-cli.config.ts
@@ -1,22 +1,19 @@
-/* eslint-disable prettier/prettier */
 import { DataSource } from 'typeorm';
+import { CreateCategoryTable1679074427751 } from './src/migrations/CreateCategoryTable-1679074427751';
 import { Category } from './src/common/entities/category.entity';
+import { AddCategories1679074427752 } from './src/migrations/AddCategories-1679074427752';
 import { SubCategory } from './src/common/entities/subCategory.entity';
 import { Business } from './src/business/entities/business.entity';
 import { User } from './src/users/entities/user.entity';
 import { Otp } from './src/otp/entities/otp.entity';
-import { AddCategories1679074427752 } from './src/migrations/AddCategories-1679074427752';
-import { CreateCategoryTable1679074427751 } from './src/migrations/CreateCategoryTable-1679074427751';
 
-export const AppDataSource = new DataSource({
+export default new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || "127.0.0.1",
-  port: parseInt(process.env.DATABASE_PORT,10) || 5432,
-  username:  process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASS || 'pass123',
-  database:  process.env.DATABASE_DB || 'postgres',
+  host: '127.0.0.1',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
   entities: [Business, Category, SubCategory, User, Otp],
-  ssl: true || false,
-  migrationsRun: true || false,
   migrations: [CreateCategoryTable1679074427751, AddCategories1679074427752],
 });
