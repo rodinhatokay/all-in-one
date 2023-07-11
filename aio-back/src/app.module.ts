a433eea22c5090cf290ecec52e264345bb3efac4
Aslan Shapso <>
2023-07-11 15:01:58 +0300

    fixing postgres things

--- aio-back/src/app.module.ts
+++ aio-back/src/app.module.ts
@@ -10,36 +10,14 @@ import { UsersModule } from "./users/user.module";
 import { AuthModule } from "./auth/auth.module";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
-import { User } from "./users/entities/user.entity";
-import { Otp } from "./otp/entities/otp.entity";
-import { readFileSync } from "fs";
-import { join } from "path";
 import { BusinessModule } from "./business/business.module";
-import { Business } from "./business/entities/business.entity";
-import { Location } from "./business/entities/location.entity";
-import { OpeningHours } from "./common/entities/openingHours.entity";
 
 @Module({
 	imports: [
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
-			useFactory: async (configService: ConfigService) => {
-				return {
-					type: "postgres",
-					host: configService.get("database.host", "127.0.0.1"),
-					port: configService.get("database.port", 5432),
-					username: configService.get("database.user", "postgres"),
-					password: configService.get("database.pass", "pass123"),
-					database: configService.get("database.db", "postgres"),
-					ssl: { // TODO: comment SSL in local mode
-						ca: readFileSync("/etc/ssl/certs/ca-certificate.crt"),
-					},
-					migrationsRun: configService.get("database.migrationsRun", true),
-					entities: [User, Otp, Business, Location, OpeningHours],
-					migrations: [join(__dirname, "/src/migrations/*.js")],
-				};
-			},
+			useFactory: (config: ConfigService) => config.get("database"),
 		}),
 		ConfigModule.forRoot({
 			validationSchema: Joi.object({
--- aio-back/src/config/app.config.ts
+++ aio-back/src/config/app.config.ts
@@ -1,16 +1,37 @@
-export default () => ({
-	environment: process.env.NODE_ENV || "development",
-	database: {
-		host: process.env.DATABASE_HOST || "127.0.0.1",
-		port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-		user: process.env.DATABASE_USER,
-		pass: process.env.DATABASE_PASS,
-		db: process.env.DATABASE_DB,
-		migrationsRun: true
-	},
-	twilio: {
-		authToken: process.env.TWILIO_AUTH_TOKEN,
-		accountSid: process.env.TWILIO_ACCOUNT_SID,
-	},
-	jwtKey: process.env.JWT_KEY,
-});
+import { readFileSync } from "fs";
+import { join } from "path";
+import { User } from "../users/entities/user.entity";
+import { Otp } from "../otp/entities/otp.entity";
+import { Business } from "../business/entities/business.entity";
+import { Location } from "../business/entities/location.entity";
+import { OpeningHours } from "../common/entities/openingHours.entity";
+
+export default () => {
+	const env = process.env.NODE_ENV || "development";
+	return {
+		environment: env,
+		database: {
+			type: "postgres",
+			host: process.env.DATABASE_HOST || "127.0.0.1",
+			port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+			username: process.env.DATABASE_USER || "postgres",
+			password: process.env.DATABASE_PASS || "pass123",
+			database: process.env.DATABASE_DB || "postgres",
+			migrationsRun: true,
+			migrations: [join(__dirname, "/src/migrations/*.js")],
+			entities: [User, Otp, Business, Location, OpeningHours],
+			ssl:
+				env === "development"
+					? undefined
+					: {
+							ca: readFileSync("/etc/ssl/certs/ca-certificate.crt"),
+					  },
+			synchronize: env === "development",
+		},
+		twilio: {
+			authToken: process.env.TWILIO_AUTH_TOKEN,
+			accountSid: process.env.TWILIO_ACCOUNT_SID,
+		},
+		jwtKey: process.env.JWT_KEY,
+	};
+};
