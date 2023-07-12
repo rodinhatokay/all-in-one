b6ae1512e03b37d75b253b362ced53b2e5d09644
RodinHatokayTe <rodinhatokay1998@gmail.com>
2023-07-12 23:53:18 +0300

    seed db

--- aio-back/src/database/database.module.ts
+++ aio-back/src/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module, DynamicModule } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { DatabaseSeederService } from "./database.seeder.service";
+import { User } from "../users/entities/user.entity";
+import { Business } from "../business/entities/business.entity";
 
 @Module({})
 export class DatabaseModule {
@@ -9,17 +11,20 @@ export class DatabaseModule {
 		return {
 			module: DatabaseModule,
 			imports: [
+				ConfigModule,
 				TypeOrmModule.forRootAsync({
 					imports: [ConfigModule],
 					inject: [ConfigService],
 					useFactory: (config: ConfigService) => {
 						const dbConfig = config.get("database");
+						console.log(dbConfig);
 						return dbConfig;
 					},
 				}),
+				TypeOrmModule.forFeature([User, Business]), // Include your entities here
 			],
 			providers: [
-				...(process.env.NODE_ENV === "development"
+				...(process.env.NODE_ENV || "development" === "development"
 					? [DatabaseSeederService]
 					: []),
 			],
--- aio-back/src/database/database.seeder.service.ts
+++ aio-back/src/database/database.seeder.service.ts
@@ -11,76 +11,82 @@ export class DatabaseSeederService implements OnApplicationBootstrap {
 	constructor(
 		@InjectRepository(User)
 		private readonly userRepository: Repository<User>,
+		@InjectRepository(Business)
 		private readonly businessRepository: Repository<Business>,
 	) {}
 
 	async onApplicationBootstrap() {
+		await this.seedBusinesses();
 		await this.seedUsers();
+	}
+
+	private async seedBusinesses() {
+		// BUILD BUSINESS
+
+		const count = await this.businessRepository.count();
+		if (count) return;
+
+		const businesses: CreateBusiness[] = [
+			{
+				name: "Fashion Emporium(Rodin)",
+				description: "Trendy clothing store with a wide selection",
+				phoneNumber: "+972524560793",
+				logoPath: "",
+				hasWhatsapp: true,
+				location: {
+					latitude: 50,
+					longitude: 20,
+				},
+				openingHours: [
+					{ day: "Monday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Thursday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Wednesday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Tuesday", hours: [{ start: "00:00", end: "04:00" }] },
+				],
+			},
+			{
+				name: "Wellness Spa(Aslan)",
+				description: "Relaxing spa with various wellness treatments",
+				phoneNumber: "+972509887021",
+				logoPath: "",
+				hasWhatsapp: false,
+				location: {
+					latitude: 50,
+					longitude: 20,
+				},
+				openingHours: [
+					{ day: "Monday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Thursday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Wednesday", hours: [{ start: "00:00", end: "04:00" }] },
+					{ day: "Tuesday", hours: [{ start: "00:00", end: "04:00" }] },
+				],
+			},
+		];
+
+		await this.businessRepository.save(businesses);
 	}
 
 	private async seedUsers() {
 		const count = await this.userRepository.count();
-		if (count === 0) {
-			// No users in the database, let's seed!
+		if (count) return;
+		// No users in the database, let's seed!
 
-			// TODO: create folder for seeding seperate of concerens
-			const user1: RegisterDto = {
-				firstName: "Rodin",
-				lastName: "Hatokay",
-				termsAccepted: true,
-				phoneNumber: "+972524560793",
-			};
+		// TODO: create folder for seeding seperate of concerens
+		const user1: RegisterDto = {
+			firstName: "Rodin",
+			lastName: "Hatokay",
+			termsAccepted: true,
+			phoneNumber: "+972524560793",
+		};
 
-			const user2: RegisterDto = {
-				firstName: "Aslan",
-				lastName: "Shapso",
-				termsAccepted: true,
-				phoneNumber: "+972509887021",
-			};
+		const user2: RegisterDto = {
+			firstName: "Aslan",
+			lastName: "Shapso",
+			termsAccepted: true,
+			phoneNumber: "+972509887021",
+		};
 
-			await this.userRepository.save(user1);
-			await this.userRepository.save(user2);
-
-			// BUILD BUSINESS
-
-			const businesses: CreateBusiness[] = [
-				{
-					name: "Fashion Emporium(Rodin)",
-					description: "Trendy clothing store with a wide selection",
-					phoneNumber: "+972524560793",
-					logoPath: "",
-					hasWhatsapp: true,
-					location: {
-						latitude: 50,
-						longitude: 20,
-					},
-					openingHours: [
-						{ day: "Monday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Thursday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Wednesday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Tuesday", hours: [{ start: "00:00", end: "04:00" }] },
-					],
-				},
-				{
-					name: "Wellness Spa(Aslan)",
-					description: "Relaxing spa with various wellness treatments",
-					phoneNumber: "+972509887021",
-					logoPath: "",
-					hasWhatsapp: false,
-					location: {
-						latitude: 50,
-						longitude: 20,
-					},
-					openingHours: [
-						{ day: "Monday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Thursday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Wednesday", hours: [{ start: "00:00", end: "04:00" }] },
-						{ day: "Tuesday", hours: [{ start: "00:00", end: "04:00" }] },
-					],
-				},
-			];
-
-			await this.businessRepository.save(businesses);
-		}
+		await this.userRepository.save(user1);
+		await this.userRepository.save(user2);
 	}
 }
