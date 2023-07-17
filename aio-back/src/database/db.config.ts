60b71845a1f79a5c1df4082b76167470b33b7dc4
Aslan Shapso <>
2023-07-17 21:49:53 +0300

    configs log

--- aio-back/src/database/db.config.ts
+++ aio-back/src/database/db.config.ts
@@ -1,23 +1,24 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { Otp } from "../otp/entities/otp.entity";
-import { Business } from "../business/entities/business.entity";
-import { Location } from "../business/entities/location.entity";
-import { OpeningHours } from "../common/entities/openingHours.entity";
-import { join } from "path";
-import { readFileSync } from "fs";
-import { User } from "../users/entities/user.entity";
-import { Category } from "../category/entities/category.entity";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Otp } from '../otp/entities/otp.entity';
+import { Business } from '../business/entities/business.entity';
+import { Location } from '../business/entities/location.entity';
+import { OpeningHours } from '../common/entities/openingHours.entity';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { User } from '../users/entities/user.entity';
+import { Category } from '../category/entities/category.entity';
 
 export const getDbConfig = (): TypeOrmModuleOptions => {
-	const env = process.env.NODE_ENV || "development";
-	const isDevMode = env.trim() === "development";
+	const env = process.env.NODE_ENV || 'development';
+	const isDevMode = env.trim() === 'development';
 
+	console.log(`isdevmode: ${isDevMode}`);
 	const commonConfig: TypeOrmModuleOptions = {
-		type: "postgres",
-		host: process.env.DATABASE_HOST || "localhost",
+		type: 'postgres',
+		host: process.env.DATABASE_HOST || 'localhost',
 		port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-		username: process.env.DATABASE_USER || "postgres",
-		password: process.env.DATABASE_PASS || "pass123",
+		username: process.env.DATABASE_USER || 'postgres',
+		password: process.env.DATABASE_PASS || 'pass123',
 		entities: [User, Otp, Business, Location, OpeningHours, Category],
 	};
 
@@ -29,13 +30,18 @@ export const getDbConfig = (): TypeOrmModuleOptions => {
 	const prodConfig: TypeOrmModuleOptions = {
 		...commonConfig,
 		migrationsRun: true,
-		migrations: [join(__dirname, "migrations/*{.ts,.js}")],
+		migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
 		ssl: {
-			ca: !isDevMode ? readFileSync("/etc/ssl/certs/ca-certificate.crt") : null,
+			ca: !isDevMode ? readFileSync('/etc/ssl/certs/ca-certificate.crt') : null,
 		},
 		synchronize: false,
 	};
 
-	return isDevMode ? devConfig : prodConfig;
+	console.log(`isdevmode: ${isDevMode}`);
+
+	const config = isDevMode ? devConfig : prodConfig;
+	console.log(`configDb:`, config);
+
+	return config;
 };
 
