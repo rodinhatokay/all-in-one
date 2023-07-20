d7498e66980219f686b54846f4529748c0329411
rodinhatokay <57593612+rodinhatokay@users.noreply.github.com>
2023-07-20 18:07:21 +0300

    Update api.ts

--- aio-front/src/services/api/api.ts
+++ aio-front/src/services/api/api.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 const KEY_STORE_TOKEN = 'StoreTokenAPI';
 
-const baseUrlApi = __DEV__
+const baseUrlApi = !__DEV__
 	? 'http://www.allinoneocean.com/api'
 	: 'http://192.168.1.107:3001/api'; // temporary local ip will fix later
 
