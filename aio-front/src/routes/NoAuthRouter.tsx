0ffe86ff3f026ce7c69c7785550acfff8b7039e8
RodinHatokayTe <rodinhatokay1998@gmail.com>
2023-07-25 17:54:12 +0300

    render terms from server

--- aio-front/src/routes/NoAuthRouter.tsx
+++ aio-front/src/routes/NoAuthRouter.tsx
@@ -1,10 +1,10 @@
-import { FC } from "react";
+import { FC } from 'react';
 // import { createStackNavigator } from '@react-navigation/stack';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NoAuthStack } from "./types";
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegsiterScreen";
-import { useLocalization } from "../contexts/LocalizationContext";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NoAuthStack } from './types';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegsiterScreen';
+import { useLocalization } from '../contexts/LocalizationContext';
 
 // const Stack = createStackNavigator();
 const Stack = createNativeStackNavigator<NoAuthStack>();
@@ -19,12 +19,12 @@ const NoAuthRouter: FC = () => {
 		>
 			<Stack.Screen
 				name="login"
-				options={{ title: "שלום" }}
+				options={{ title: 'שלום' }}
 				component={LoginScreen}
 			/>
 			<Stack.Screen
 				name="register"
-				options={{ headerShown: true, title: t("register") }}
+				options={{ headerShown: true, title: t('register') }}
 				component={RegisterScreen}
 			/>
 		</Stack.Navigator>
