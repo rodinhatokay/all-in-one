d5f83ce82a53d29c24b7072ed4e9c409feac01f9
Aslan Shapso <>
2023-07-29 15:47:23 +0300

    adding migration file of add foreign keys to user

---
+++ aio-back/src/migrations/1690634796121-AddForeignKeysToUserFavoriteBusinesses.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddForeignKeysToUserFavoriteBusinesses1631303405982 implements MigrationInterface {
+    name = 'AddForeignKeysToUserFavoriteBusinesses1631303405982'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      // Add foreign key constraints to the existing table
+      await queryRunner.query(`
+        ALTER TABLE "user_favorite_businesses"
+        ADD CONSTRAINT "FK_user_favorite_businesses_userId_user" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE
+      `);
+  
+      await queryRunner.query(`
+        ALTER TABLE "user_favorite_businesses"
+        ADD CONSTRAINT "FK_user_favorite_businesses_businessId_business" FOREIGN KEY ("businessId") REFERENCES "business"("id") ON DELETE CASCADE ON UPDATE CASCADE
+      `);
+    }
+  
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      // Remove foreign key constraints from the existing table
+      await queryRunner.query(`
+        ALTER TABLE "user_favorite_businesses"
+        DROP CONSTRAINT "FK_user_favorite_businesses_userId_user"
+      `);
+  
+      await queryRunner.query(`
+        ALTER TABLE "user_favorite_businesses"
+        DROP CONSTRAINT "FK_user_favorite_businesses_businessId_business"
+      `);
+    }
+  }
+  
