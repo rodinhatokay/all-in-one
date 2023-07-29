7963915b1e554b2144ef65f96bae49e1f6bbadd0
Aslan Shapso <>
2023-07-29 21:27:02 +0300

    last fix

---
+++ aio-back/src/migrations/1631303405984-AddForeignKeysToUserFavoriteBusinesses.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddForeignKeysToUserFavoriteBusinesses1631303405984 implements MigrationInterface {
+    name = 'AddForeignKeysToUserFavoriteBusinesses1631303405984'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+          -- Create the "favorite_businesses" table
+          CREATE TABLE "favorite_businesses" (
+              "userId" uuid NOT NULL,
+              "businessId" uuid NOT NULL,
+              CONSTRAINT "PK_8e00b134db84f47b551b4e083f6" PRIMARY KEY ("userId", "businessId")
+          );
+    
+          -- Create foreign keys
+          ALTER TABLE "favorite_businesses" ADD CONSTRAINT "FK_9e32ae26a81c7cf6dcecaa27b56" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE;
+          ALTER TABLE "favorite_businesses" ADD CONSTRAINT "FK_50256e61c526b65b58a6ef5eb45" FOREIGN KEY ("businessId") REFERENCES "business"("id") ON DELETE CASCADE;
+        `);
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+          -- Drop foreign keys
+          ALTER TABLE "favorite_businesses" DROP CONSTRAINT "FK_9e32ae26a81c7cf6dcecaa27b56";
+          ALTER TABLE "favorite_businesses" DROP CONSTRAINT "FK_50256e61c526b65b58a6ef5eb45";
+    
+          -- Drop the "favorite_businesses" table
+          DROP TABLE "favorite_businesses";
+        `);
+      }
+}
