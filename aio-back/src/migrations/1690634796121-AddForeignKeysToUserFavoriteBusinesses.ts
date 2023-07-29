5dbc5accdf79ce2a607098f83033ff9c99eb62d5
Aslan Shapso <>
2023-07-29 21:17:11 +0300

    fixing migrations for forieng kkeys

--- aio-back/src/migrations/1690634796121-AddForeignKeysToUserFavoriteBusinesses.ts
+++ aio-back/src/migrations/1690634796121-AddForeignKeysToUserFavoriteBusinesses.ts
@@ -7,13 +7,13 @@ export class AddForeignKeysToUserFavoriteBusinesses1631303405982 implements Migr
         await queryRunner.query(`
           -- Create the "favorite_businesses" table
           CREATE TABLE "favorite_businesses" (
-              "user_id" uuid NOT NULL,
+              "userId" uuid NOT NULL,
               "business_id" uuid NOT NULL,
-              CONSTRAINT "PK_8e00b134db84f47b551b4e083f6" PRIMARY KEY ("user_id", "business_id")
+              CONSTRAINT "PK_8e00b134db84f47b551b4e083f6" PRIMARY KEY ("userId", "business_id")
           );
     
           -- Create foreign keys
-          ALTER TABLE "favorite_businesses" ADD CONSTRAINT "FK_9e32ae26a81c7cf6dcecaa27b56" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE;
+          ALTER TABLE "favorite_businesses" ADD CONSTRAINT "FK_9e32ae26a81c7cf6dcecaa27b56" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE;
           ALTER TABLE "favorite_businesses" ADD CONSTRAINT "FK_50256e61c526b65b58a6ef5eb45" FOREIGN KEY ("business_id") REFERENCES "business"("id") ON DELETE CASCADE;
         `);
       }
