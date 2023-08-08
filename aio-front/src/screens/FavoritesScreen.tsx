ae516bc1220df00820aec638213d3d4e18160deb
RodinHatokayTe <rodinhatokay1998@gmail.com>
2023-08-08 23:42:41 +0300

    fix(web): favorite tabs crash

--- aio-front/src/screens/FavoritesScreen.tsx
+++ aio-front/src/screens/FavoritesScreen.tsx
@@ -1,8 +1,6 @@
 import { View } from 'react-native';
 import { StyleSheet } from 'react-native';
 // import Loader from '../components/Loader/Loader';
-import { Business } from '../services/business/business.types';
-import BusinessCard from '../components/BusinessCard/BusinessCard';
 import EmptyFavorites from '../sections/Favorites/EmptyFavorites';
 import BusinessList from '../components/BusinessList';
 import { useBusinessFavorites } from '../contexts/BusinessFavoritesContext';
@@ -11,19 +9,13 @@ const FavoritesScreen = () => {
 	const { favoritesQuery } = useBusinessFavorites();
 	const { data: favoritesList, isLoading } = favoritesQuery;
 
-	const renderItem = ({ item }: { item: Business }) => (
-		<BusinessCard business={item} />
-	);
-
 	if (isLoading) return null;
 
 	return (
 		<View style={styles.flex}>
 			<BusinessList
 				data={favoritesList}
-				renderItem={renderItem}
 				style={styles.flex}
-				keyExtractor={(item) => item.id}
 				ListEmptyComponent={EmptyFavorites}
 			/>
 		</View>
