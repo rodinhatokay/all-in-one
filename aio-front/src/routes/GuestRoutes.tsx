2c5833b352ddb55423e7ac2b838e50f64d4cc9f9
Rodin Hatokay <57593612+rodinhatokay@users.noreply.github.com>
2023-08-10 16:06:35 +0300

    feat: allow guests to view businesses (#58)

--- aio-front/src/routes/GuestRoutes.tsx
+++ aio-front/src/routes/GuestRoutes.tsx
@@ -1,25 +1,27 @@
 import { FC } from 'react';
 // import { createStackNavigator } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NoAuthStack } from './types';
+import { GuestRoute } from './types';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegsiterScreen';
 import { useLocalization } from '../contexts/LocalizationContext';
+import HomeStackScreen from './HomeStackScreen';
 
 // const Stack = createStackNavigator();
-const Stack = createNativeStackNavigator<NoAuthStack>();
+const Stack = createNativeStackNavigator<GuestRoute>();
 
-const NoAuthRouter: FC = () => {
+const GuestRoutes: FC = () => {
 	const { t } = useLocalization();
 	return (
 		<Stack.Navigator
-			screenOptions={{
-				headerShown: false,
-			}}
+			// screenOptions={{
+			// 	headerShown: false,
+			// }}
+			initialRouteName="homeStack"
 		>
 			<Stack.Screen
 				name="login"
-				options={{ title: 'שלום' }}
+				options={{ title: t('login'), headerShown: true }}
 				component={LoginScreen}
 			/>
 			<Stack.Screen
@@ -27,8 +29,13 @@ const NoAuthRouter: FC = () => {
 				options={{ headerShown: true, title: t('register') }}
 				component={RegisterScreen}
 			/>
+			<Stack.Screen
+				name="homeStack"
+				options={{ headerShown: false, title: t('home') }}
+				component={HomeStackScreen}
+			/>
 		</Stack.Navigator>
 	);
 };
 
-export default NoAuthRouter;
+export default GuestRoutes;
